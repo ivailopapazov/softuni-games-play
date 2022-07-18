a57c305f95fb2a3c7b16ba843c10f1087112508e
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-07-18 19:47:56 +0300

    Add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Routes, Route} from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import CreateGame from './components/CreateGame/CreateGame';
+import Catalog from './components/Catalog/Catalog';
 import './App.css';
 
 function App() {
@@ -13,103 +17,14 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/create" element={<CreateGame />} />
+                    <Route path="/catalog" element={<Catalog />} />
                 </Routes>
             </main>
+
             
-            {/* Login Page ( Only for Guest users ) */}
-            {/* <section id="login-page" className="auth">
-                <form id="login">
-                    <div className="container">
-                        <div className="brand-logo" />
-                        <h1>Login</h1>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Sokka@gmail.com"
-                        />
-                        <label htmlFor="login-pass">Password:</label>
-                        <input type="password" id="login-password" name="password" />
-                        <input type="submit" className="btn submit" defaultValue="Login" />
-                        <p className="field">
-                            <span>
-                                If you don't have profile click <a href="#">here</a>
-                            </span>
-                        </p>
-                    </div>
-                </form>
-            </section> */}
-            {/* Register Page ( Only for Guest users ) */}
-            {/* <section id="register-page" className="content auth">
-                <form id="register">
-                    <div className="container">
-                        <div className="brand-logo" />
-                        <h1>Register</h1>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="maria@email.com"
-                        />
-                        <label htmlFor="pass">Password:</label>
-                        <input type="password" name="password" id="register-password" />
-                        <label htmlFor="con-pass">Confirm Password:</label>
-                        <input type="password" name="confirm-password" id="confirm-password" />
-                        <input className="btn submit" type="submit" defaultValue="Register" />
-                        <p className="field">
-                            <span>
-                                If you already have profile click <a href="#">here</a>
-                            </span>
-                        </p>
-                    </div>
-                </form>
-            </section> */}
-            {/* Create Page ( Only for logged-in users ) */}
-            {/* <section id="create-page" className="auth">
-                <form id="create">
-                    <div className="container">
-                        <h1>Create Game</h1>
-                        <label htmlFor="leg-title">Legendary title:</label>
-                        <input
-                            type="text"
-                            id="title"
-                            name="title"
-                            placeholder="Enter game title..."
-                        />
-                        <label htmlFor="category">Category:</label>
-                        <input
-                            type="text"
-                            id="category"
-                            name="category"
-                            placeholder="Enter game category..."
-                        />
-                        <label htmlFor="levels">MaxLevel:</label>
-                        <input
-                            type="number"
-                            id="maxLevel"
-                            name="maxLevel"
-                            min={1}
-                            placeholder={1}
-                        />
-                        <label htmlFor="game-img">Image:</label>
-                        <input
-                            type="text"
-                            id="imageUrl"
-                            name="imageUrl"
-                            placeholder="Upload a photo..."
-                        />
-                        <label htmlFor="summary">Summary:</label>
-                        <textarea name="summary" id="summary" defaultValue={""} />
-                        <input
-                            className="btn submit"
-                            type="submit"
-                            defaultValue="Create Game"
-                        />
-                    </div>
-                </form>
-            </section> */}
             {/* Edit Page ( Only for the creator )*/}
             {/* <section id="edit-page" className="auth">
                 <form id="edit">
@@ -136,8 +51,6 @@ function App() {
                 </form>
             </section> */}
 
-
-            
             {/*Details Page*/}
             {/* <section id="game-details">
                 <h1>Game Details</h1>
@@ -194,41 +107,6 @@ function App() {
                         />
                     </form>
                 </article>
-            </section> */}
-            {/* Catalogue */}
-            {/* <section id="catalog-page">
-                <h1>All Games</h1>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="./images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>Cover Fire</h2>
-                        <a href="#" className="details-button">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="./images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>Zombie lang</h2>
-                        <a href="#" className="details-button">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="./images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>MineCraft</h2>
-                        <a href="#" className="details-button">
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <h3 className="no-articles">No articles yet</h3>
             </section> */}
         </div>
     );
