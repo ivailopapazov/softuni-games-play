3f06ef20cbe85d0fcaf2dac947a7332ecba79148
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-07-18 21:03:21 +0300

    Add link to details to latest game

--- src/components/Home/LatestGame/LatestGame.js
+++ src/components/Home/LatestGame/LatestGame.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const LatestGame = ({
     game
 }) => {
@@ -15,9 +17,9 @@ const LatestGame = ({
                 <span>☆</span>
             </div>
             <div className="data-buttons">
-                <a href="#" className="btn details-btn">
+                <Link to={`/catalog/${game._id}`} className="btn details-btn">
                     Details
-                </a>
+                </Link>
             </div>
         </div>
     );
