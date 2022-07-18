0bb4491c85fb7588f327f0489d9b097f75c9050b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-07-18 19:40:19 +0300

    Change navigation components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <header>
-        {/* Navigation */}
-        <h1>
-          <a className="home" href="#">
-            GamesPlay
-          </a>
-        </h1>
-        <nav>
-          <a href="#">All games</a>
-          {/* Logged-in users */}
-          <div id="user">
-            <a href="#">Create Game</a>
-            <a href="#">Logout</a>
-          </div>
-          {/* Guest users */}
-          <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-          </div>
-        </nav>
-      </header>
+            <h1>
+                <Link className="home" to="/">
+                    GamesPlay
+                </Link>
+            </h1>
+            <nav>
+                <Link to="/catalog">All games</Link>
+                {/* Logged-in users */}
+                <div id="user">
+                    <Link to="/create">Create Game</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+                {/* Guest users */}
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </nav>
+        </header>
     );
 };
 
