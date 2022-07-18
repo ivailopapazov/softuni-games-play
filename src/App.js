1107c1b52f01e91d8368d854d117972b28972e91
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2022-07-18 22:16:30 +0300

    Add firebase to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, lazy, Suspense } from "react";
 import { Routes, Route, useNavigate} from 'react-router-dom';
 import uniqid from 'uniqid';
 
@@ -7,11 +7,12 @@ import * as gameService from './services/gameService';
 import Header from './components/Header/Header';
 import Home from './components/Home';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import CreateGame from './components/CreateGame/CreateGame';
 import Catalog from './components/Catalog/Catalog';
 import GameDetails from "./components/GameDetails/GameDetails";
 import './App.css';
+
+const Register = lazy(() => import('./components/Register/Register'));
 
 function App() {
     const [games, setGames] = useState([]);
@@ -60,7 +61,11 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home games={games}/>} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+                    <Route path="/register" element={
+                        <Suspense fallback={<span>Loading....</span>}>
+                            <Register />
+                        </Suspense>
+                    } />
                     <Route path="/create" element={<CreateGame addGameHandler={addGameHandler} />} />
                     <Route path="/catalog" element={<Catalog games={games}/>} />
                     <Route path="/catalog/:gameId" element={<GameDetails games={games} addComment={addComment} />} />
